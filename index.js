f11d5887634745e295c992da599a1bb5805b0221
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-09-19 21:46:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,163 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+// Get JWT Token
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Aceess' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+    })
+    next();
+}
+
+
+
+const uri = "mongodb+srv://MyEcommerce:VNOWeYTYMXxnEwyE@cluster0.wlhaaxa.mongodb.net/?retryWrites=true&w=majority";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlhaaxa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+const verifyAdmin = async (req, res, next) => {
+    const requester = req.decoded.email;
+    const requesterAccount = await usersCollection.findOne({ email: requester });
+    if (requesterAccount.role === 'admin') {
+        next();
+    }
+    else {
+        res.status(403).send({ message: 'forbidden' });
+    }
+}
+
+
+const run = async () => {
+    try {
+
+        await client.connect();
+
+        const usersCollection = client.db("MyEcommerce").collection("Users");
+        const productsCollection = client.db("MyEcommerce").collection("Products");
+
+
+        // post user by email
+        app.put('/user', async (req, res) => {
+            const user = req.body;
+            const email = user?.email;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ result, accessToken: token });
+        })
+
+
+        // get users
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = usersCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users)
+        })
+
+
+        // delete an user
+        app.delete('/user/:id', verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
+        })
+
+
+        // post admin by email
+        app.put('/user/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: { role: 'admin' }
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+        //get admin api 
+        app.get("/user/isAdmin/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const adminUser = await usersCollection.findOne(query);
+            const isAdmin = adminUser.role === "admin"
+            res.send({ role: isAdmin })
+        })
+
+
+        // remove admin by email
+        app.put('/user/admin/remove/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: { role: '' }
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+        // get admin
+        app.get('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user?.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
+
+        // get products data api 
+        app.get("/products", async (req, res) => {
+            const query = {}
+            const products = await productsCollection.find(query).toArray();
+            res.send(products)
+        })
+
+        // get product api data 
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product)
+        })
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Running My E-Commerce Server");
+});
+app.listen(port, () => {
+    console.log("Listen to Port", port);
+});
